795e29c5143f538ddd1c9ec4f12737d684318f25
Agnes Coaker <agnescoaker@gmail.com>
2023-05-08 20:23:43 +0100

    mooore formatting fixes

--- pages/FAQs.py
+++ pages/FAQs.py
@@ -17,12 +17,12 @@ st.write("""The topics were generated from the text using a method called \'Late
          together. We begin by assuming there are a number of topics, in this case 20, and that the distribution of words
          within a topic, and the distribution of topics within a text both follow Dirichlet distributions. The model then
          uses the observed frequencies to fit the distributions and thus deduces the potential topics. The titles of these 
-         topics must be assigned manually and this was done by the author. \n
-         After this has been done, each text/piece of text (in this case each article within the FTA) has a topic (or topics) 
+         topics must be assigned manually and this was done by the author."""
+st.write("""After this has been done, each text/piece of text (in this case each article within the FTA) has a topic (or topics) 
          associated with it. Alongside these are probabilities that indicated, based on the observed frequencies, how likely an
          article is to belong to a topic. The slider in the summariser allows a minimum limit for this probability to be set. 
          At 0, any article that may belong to that topic, even if it has a low probability of doing so, will be summarised. 
-         Conversely, at 1, only articles that definetely belong to a topic are summarised. """)
+         Conversely, at 1, only articles that definetely belong to a topic are summarised.""")
 
 st.subheader('How is the text summarised?')
 st.write("""Streamlit is **_really_ cool**.""")
