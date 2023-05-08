907c06113e817a755ccb6c35dd65bc787190a072
Agnes Coaker <agnescoaker@gmail.com>
2023-05-08 20:08:14 +0100

    start explanation page

---
+++ pages/FAQs.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import csv
+import pickle
+
+st.set_page_config(
+    page_title="UK-AU FTA Summary",
+)
+
+st.title('FAQs - Summarise the UK-AU FTA')
+
+st.subheader('Where do the topics come from?')
+st.markdown('Streamlit is **_really_ cool**.')
+
+st.subheader('Where do the topics come from?')
+st.markdown('Streamlit is **_really_ cool**.')
+
+st.subheader('Where do the topics come from?')
+st.markdown('Streamlit is **_really_ cool**.')
