948b0f77746c51110e538e19a4d2edbb0c8bb5b9
Agnes Coaker <agnescoaker@gmail.com>
2023-05-08 20:01:44 +0100

    update app so that summaries can be piped in

--- summary_app.py
+++ summary_app.py
@@ -20,29 +20,10 @@ topic = st.selectbox(
     list(topics.values()))
 #create slider to allow probablility to be selected
 prob = st.slider('Select the lowest probability that text relates to this topic before it is summarised.', min_value = 0.0,
-                           max_value = 1.0, value = 0.9, step = 0.01)
+                           max_value = 1.0, value = 0.9, step = 0.2)
 
-## read in data to be used 
-articles_df = pd.read_csv('article_topics.csv')
-#filter to get articles relevant to user selection
-t = list(topics.keys())[list(topics.values()).index(topic)]
-articles_to_sum = articles_df[articles_df[('prob_t' + str(t))] >= prob]
-#put text into one string and then split out according to length limits
-article_text = ' '.join(articles_to_sum['Article Text'])
-def summary_chunks(text):
-    chunks = []
-    words = text.split()
-    for i in range(0, len(words), 700):
-      chunks.append(" ".join(words[i:i+700]))
-    return chunks
-article_text_chunks = summary_chunks(article_text)
-
-## summarise the chunks
-from transformers import BartForConditionalGeneration, BartTokenizer, BartConfig
-tokeniser = BartTokenizer.from_pretrained('facebook/bart-large-cnn')
-model = BartForConditionalGeneration.from_pretrained('facebook/bart-large-cnn')
-inputs = [tokeniser.batch_encode_plus([text],return_tensors='pt') for text in article_text_chunks]
-summary_ids = [model.generate(i['input_ids'], early_stopping=True) for i in inputs]
+## read in summaries from CSV
+#then just need to select correct one according to use input
 
 
 
@@ -52,9 +33,9 @@ summary_ids = [model.generate(i['input_ids'], early_stopping=True) for i in inpu
 
 #filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(article_text_chunks)
+#if st.checkbox('Show raw data'):
+#    st.subheader('Raw data')
+#    st.write(article_text_chunks)
 
 #st.subheader('Number of pickups by hour')
 #hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
