b86efb81b7422c099cde4e08b5a4d3f3204fee08
Agnes Coaker <agnescoaker@gmail.com>
2023-05-18 16:05:40 +0100

    get app working correctly and get in final output from ipynb file

--- Summariser.py
+++ Summariser.py
@@ -18,9 +18,11 @@ with open('topic_dict.pkl', 'rb') as f:
 topic = st.selectbox(
     'Select a topic to see a summary for.',
     list(topics.values()))
+t = list(topics.keys())[list(topics.values()).index(topic)]
+
 #create slider to allow probablility to be selected
-prob = st.slider('Select the lowest probability that text relates to this topic before it is summarised.', min_value = 0.0,
-                           max_value = 1.0, value = 0.8, step = 0.2)
+#prob = st.slider('Select the lowest probability that text relates to this topic before it is summarised.', min_value = 0.0,
+#                           max_value = 1.0, value = 0.8, step = 0.2)
 
 ## read in summaries from CSV
 #then just need to select correct one according to use input
@@ -28,12 +30,18 @@ prob = st.slider('Select the lowest probability that text relates to this topic 
 
 st.subheader('Top Level Summary')
 #abstrative summarisation here
+with open('summaries.pkl', 'rb') as f:
+    summaries = pickle.load(f)
+st.markdown(summaries[t])
 
 st.subheader('In Depth Summary')
 #summaries from chunks here
+with open('pre_recursive_summaries.pkl', 'rb') as f:
+    pre_recursive_summaries = pickle.load(f)
+st.markdown(pre_recursive_summaries[t])
 
 st.subheader('Source')
-st.text('The summaries regarding "' + str(topic) + '" are sources from the\nfollowing chapters and articles in the FTA.')
+st.markdown('The summaries regarding "' + str(topic) + '" are sources from the\nfollowing chapters and articles in the FTA.')
 #list chapters and articles here
 
 
